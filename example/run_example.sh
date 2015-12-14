cb6739085f88c8bccf6d44b93f44b1040f42a494
Brian Bishop <brian@employeesm-27339.jcvi.org>
2015-12-14 18:52:44 -0500

    rename

--- example/run_example.sh
+++ example/run_example.sh
@@ -3,4 +3,4 @@
 export basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export bin_dir=$(dirname ${basedir})/bin
 
-${bin_dir}/ISmapper.pl --genome UH02_contigs_annotation.fa --is ISAba1.fasta --reference ACICU.fasta --out output 
+${bin_dir}/ISseeker.pl --genome UH02_contigs_annotation.fa --is ISAba1.fasta --reference ACICU.fasta --out output 
